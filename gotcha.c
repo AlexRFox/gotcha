13f0755432d7f503add6b05c6b7716f68e968acc
Alex Willisson <atw@mit.edu>
2012-03-25 03:50:33 -0400

    added readme and gpl

--- gotcha.c
+++ gotcha.c
@@ -1,14 +1,28 @@
+/* This program is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+
 #include <X11/Intrinsic.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/time.h>
 
-typedef struct _X11LogSess {
+typedef struct _x11sess {
 	Display *dpy;
 	Window focuswin;
 	FILE *fd;
-} X11LogSess;
+} x11sess;
 
 void gotcha (void);
 double get_secs (void);
@@ -41,15 +55,15 @@ get_secs (void)
 int
 main (int argc, char **argv)
 {
-	X11LogSess *sess;
+	x11sess *sess;
 	FILE *fd;
 	double now = get_secs ();
 	double last_time = now;
 	XEvent ev;
 	int revert_to_return;
     
-	if (!(sess = malloc (sizeof (struct _X11LogSess)))) {
-		fprintf(stderr, ">> Error: %s\n", strerror(errno));
+	if (!(sess = malloc (sizeof (struct _x11sess)))) {
+		fprintf (stderr, "error allocating memory");
 		sess = NULL;
 	} else {
 		sess->dpy = XOpenDisplay (getenv ("DISPLAY"));
@@ -62,7 +76,7 @@ main (int argc, char **argv)
 	}
     
 	if (sess == NULL) {
-		fprintf (stderr, "Something went wrong. Aborting.\n");
+		fprintf (stderr, "something broke, oops.");
 		exit (1);
 	}
     
